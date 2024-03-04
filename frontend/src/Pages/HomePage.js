7ec4f3ae3cb7513e94f4115b6d85af515af7c392
Mainak <mainak9990@gmail.com>
2024-03-05 02:11:18 +0530

    mongodb connected successfully

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Container, Box, Text, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import Login from '../components/Authentication/Login';
+import SignUp from '../components/Authentication/SignUp';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW="xl" centerContent>
+      <Box display="flex" justifyContent="center" p={3} bg={"white"}
+        w="100%" m="40px 0 15px 0" borderRadius="lg" borderWidth="1px">
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px" color="black">
+        <Tabs variant='soft-rounded'>
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
